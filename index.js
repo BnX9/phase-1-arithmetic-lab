5bd39d06a8b98ad2e2cf44117c17a1710c560b1b
Brian Ngugi <brianngugi.sw@gmail.com>
2023-09-19 13:06:30 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
 // Write your code here
+// equation whose multiplied results will equal 62
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+
+
+// generates a randon integer greater than 0:
+let num = 0
+let random = Math.floor(Math.random()) +1
+
+
+
+
+
+// an equation that calculates a remainder that equals to 4
+let num3 = 22
+let num4 = 9
+let mod = num3 % num4
+
+
+
+
+
+
+
+//will return 20 as the highest number in the set
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
